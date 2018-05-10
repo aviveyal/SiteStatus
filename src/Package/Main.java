2a61043da18901ebe6d152b0a4a2cda4af71a551
Aviv Eyal <204518997@stud.colman.ac.il>
2018-05-10 20:03:58 +0300

    saved 10.5

--- src/Package/Main.java
+++ src/Package/Main.java
@@ -1,6 +1,5 @@
 package Package;
 
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -55,25 +54,22 @@ public class Main implements Initializable {
 	BorderPane borderPane;
 	@FXML
 	Label label;
-	
+
 	@FXML
 	DatePicker DatePickerFrom;
 	@FXML
 	ProgressIndicator progress;
-	
+
 	@FXML
 	DatePicker DatePickerTo;
 	@FXML
 	ChoiceBox choiceBox;
-	
-	
-	
 
 	ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
 	@FXML
 	Label caption = new Label("");
-	
-	Double total=0.0;
+
+	Double total = 0.0;
 	String from;
 	Double dateDifferance;
 	String to;
@@ -81,105 +77,98 @@ public class Main implements Initializable {
 	String secondDate = "";
 	boolean flag = false;
 	Double timing = 0.0;
-	Date checkDate ;
+	Date checkDate;
 	ArrayList<String> Sites = new ArrayList<String>();
-	
-		
+
 	HashMap<String, Double> map = new HashMap<String, Double>();
 	// ArrayList transactionList = new ArrayList<>();
 
 	JSONParser parser = new JSONParser();
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		choiceBox.getItems().add("Haifa bay port");
-        choiceBox.getItems().add("Intel - FAB 28");
-        choiceBox.getItems().add("S32 Mine");
-        choiceBox.getItems().add("S32 Refinery");
-        choiceBox.getItems().add("BHP Area C");
-        choiceBox.getItems().add("BHP San Manuel");
-        choiceBox.getItems().add("Minera Centinela");
-        choiceBox.getItems().add("Vale NC1");
-        choiceBox.getItems().add("Vale NC2");
-        
-        
-        Sites.add("705");
-        Sites.add("704");
-        Sites.add("706");
-        Sites.add("708");
-        Sites.add("707");
-        Sites.add("723");
-        Sites.add("737");
-        Sites.add("715");
-        Sites.add("714");
-        //progress.setVisible(false);
-        
-        
-		
-	}
-	
 
-	
-	public void searchButton(){
-		
-		String Site="";
+		choiceBox.getItems().add("Haifa bay port");
+		choiceBox.getItems().add("Intel - FAB 28");
+		choiceBox.getItems().add("S32 Mine");
+		choiceBox.getItems().add("S32 Refinery");
+		choiceBox.getItems().add("BHP Area C");
+		choiceBox.getItems().add("BHP San Manuel");
+		choiceBox.getItems().add("Minera Centinela");
+		choiceBox.getItems().add("Vale NC1");
+		choiceBox.getItems().add("Vale NC2");
+
+		Sites.add("705");
+		Sites.add("704");
+		Sites.add("706");
+		Sites.add("708");
+		Sites.add("707");
+		Sites.add("723");
+		Sites.add("737");
+		Sites.add("715");
+		Sites.add("714");
+		// progress.setVisible(false);
+
+	}
+
+	public void searchButton() {
+
+		String Site = "";
 		resetVariables();
-		switch (choiceBox.getSelectionModel().getSelectedItem().toString()){
+		switch (choiceBox.getSelectionModel().getSelectedItem().toString()) {
 		case "Haifa bay port":
-			Site="705";
+			Site = "705";
 			break;
 		case "Intel - FAB 28":
-			Site="704";
+			Site = "704";
 			break;
 		case "S32 Mine":
-			Site="706";
+			Site = "706";
 			break;
 		case "S32 Refinery":
-			Site="708";
+			Site = "708";
 			break;
 		case "BHP Area C":
-			Site="707";
+			Site = "707";
 			break;
 		case "BHP San Manuel":
-			Site="723";
+			Site = "723";
 			break;
 		case "Minera Centinela":
-			Site="737";
+			Site = "737";
 			break;
 		case "Vale NC1":
-			Site="715";
+			Site = "715";
 			break;
 		case "Vale NC2":
-			Site="714";
+			Site = "714";
 			break;
-			
-		}
-		
-		//System.out.println(DatePickerFrom.getValue());
-		GraphGenerate(Date.from(DatePickerFrom.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()) , 
-				Date.from(DatePickerTo.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()),Site );
 
-		loadPieChart();	
+		}
+
+		// System.out.println(DatePickerFrom.getValue());
+		GraphGenerate(Date.from(DatePickerFrom.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()),
+				Date.from(DatePickerTo.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()), Site);
+
+		loadPieChart();
 	}
-	
-	public void resetVariables(){
+
+	public void resetVariables() {
 		pieChart.getData().clear();
 		pieChartData.clear();
 		map.clear();
-		total=0.0;
-		timing=0.0;
+		total = 0.0;
+		timing = 0.0;
 		firstDate = "";
 		secondDate = "";
 		flag = false;
 	}
-	
 
-	public void GraphGenerate(Date PickerFrom ,Date PickerTo , String site ) {
+	public void GraphGenerate(Date PickerFrom, Date PickerTo, String site) {
 		System.out.println(site);
-			timing=0.0;	
+		timing = 0.0;
 		try {
-			Object obj = parser.parse(new FileReader("./tickets/"+site+".json"));
+			Object obj = parser.parse(new FileReader("./tickets/" + site + ".json"));
 			JSONObject jsonObject = (JSONObject) obj;
 			JSONObject changelog = (JSONObject) jsonObject.get("changelog");
 			JSONArray histories = (JSONArray) changelog.get("histories");
@@ -193,37 +182,45 @@ public class Main implements Initializable {
 						JSONObject item = (JSONObject) items.get(j);
 						String field = (String) item.get("field");
 						if (field.equals("status")) {
-							
-							checkDate = new SimpleDateFormat("yyyy-MM-dd").parse(((String) history.get("created")).split("T",2)[0]); //change format to date from json
-						if((checkDate.after(PickerFrom) || checkDate.equals(PickerFrom)) && ( checkDate.before(PickerTo)|| checkDate.equals(PickerTo))) //check dates range 
-						{
-							if (!flag) {
-								firstDate = (String) history.get("created");
-								flag = true;
-							} else {
-								secondDate = (String) history.get("created");
-								from = (String) item.get("fromString");
-								
-								//System.out.println(from);
-								if (map.containsKey(from)) {
-									timing = map.get(from);
-									map.put(from, timing + calculateDateDifferance(secondDate, firstDate));
-								} else {
-									map.put(from, calculateDateDifferance(secondDate, firstDate));
-								}
 
-								flag = true;
-								firstDate = secondDate;
+							checkDate = new SimpleDateFormat("yyyy-MM-dd")
+									.parse(((String) history.get("created")).split("T", 2)[0]); // change
+																								// format
+																								// to
+																								// date
+																								// from
+																								// json
+							if ((checkDate.after(PickerFrom) || checkDate.equals(PickerFrom))
+									&& (checkDate.before(PickerTo) || checkDate.equals(PickerTo))) // check
+																									// dates
+																									// range
+							{
+								if (!flag) {
+									firstDate = (String) history.get("created");
+									flag = true;
+								} else {
+									secondDate = (String) history.get("created");
+									from = (String) item.get("fromString");
+
+									// System.out.println(from);
+									if (map.containsKey(from)) {
+										timing = map.get(from);
+										map.put(from, timing + calculateDateDifferance(secondDate, firstDate));
+									} else {
+										map.put(from, calculateDateDifferance(secondDate, firstDate));
+									}
+
+									flag = true;
+									firstDate = secondDate;
+								}
+								from = (String) item.get("fromString");
+								to = (String) item.get("toString");
 							}
-							from = (String) item.get("fromString");
-							to = (String) item.get("toString");
-						}
 						}
 					}
 				}
 			}
 
-					
 		} catch (FileNotFoundException fe) {
 			fe.printStackTrace();
 		} catch (Exception e) {
@@ -240,13 +237,17 @@ public class Main implements Initializable {
 		bd = bd.setScale(places, RoundingMode.HALF_UP);
 		return bd.doubleValue();
 	}
-	
-	public void loadPieChart(){
+
+	public void loadPieChart() {
 		System.out.println("Loading charts...");
-		total=0.0;		
+		total = 0.0;
 		for (String key : map.keySet()) {
-			pieChartData.add(new PieChart.Data(key, round(map.get(key), 2))); // add data to pie chart
-			total= total+ round(map.get(key), 5);
+			pieChartData.add(new PieChart.Data(key, round(map.get(key), 2))); // add
+																				// data
+																				// to
+																				// pie
+																				// chart
+			total = total + round(map.get(key), 5);
 			System.out.println(key + " - " + map.get(key));
 
 		}
@@ -257,33 +258,37 @@ public class Main implements Initializable {
 		pieChart.setTitle("Site Activity");
 		pieChartData.forEach(data -> data.nameProperty()
 				.bind(Bindings.concat(data.getName(), " for ", data.pieValueProperty(), " Hours")));
-		
-		
-		//set mpuse over functions
+
+		// set mouse over functions
 		pieChart.getData().stream().forEach(data -> {
 			data.getNode().addEventHandler(MouseEvent.MOUSE_ENTERED, (e) -> {
-				label.setVisible(true);
-				label.setText(round(((data.getPieValue() *100 )/total),2) +"%");	
+
+				label.setMouseTransparent(true);
+				label.setText(round(((data.getPieValue() * 100) / total), 2) + "%");
 				label.setTranslateX(e.getSceneX());
 				label.setTranslateY(e.getSceneY() + 20);
-				data.getNode().addEventHandler(MouseEvent.MOUSE_EXITED, (x) -> {
-					label.setVisible(false);
-					
-
-				});					
-
+				label.setVisible(true);
 			});
-		});
-		
-		pieChart.getData().stream().forEach(data -> {
-			data.getNode().addEventHandler(MouseEvent.MOUSE_EXITED, (e) -> {
+
+			data.getNode().addEventHandler(MouseEvent.MOUSE_EXITED, (x) -> {
 				label.setVisible(false);
-				
-
+				// Need to add a event when the mouse move hover the slice
+				// If I don't the popup stay blocked on edges of the slice.
 			});
+
+			data.getNode().addEventHandler(MouseEvent.MOUSE_MOVED, (y) -> {
+				{
+					// Keep Label near the mouse
+					label.setTranslateX(y.getSceneX());
+					label.setTranslateY(y.getSceneY() + 20);
+				}
+			});
+
 		});
-		
-		//borderPane.getChildren().add(pieChart);
+
+		;
+
+		// borderPane.getChildren().add(pieChart);
 	}
 
 	public static Double calculateDateDifferance(String Date1, String Date2) throws ParseException {
@@ -300,12 +305,12 @@ public class Main implements Initializable {
 
 		finalDate1 = DateTime1[0] + " " + DateTime1[1];
 		finalDate2 = DateTime2[0] + " " + DateTime2[1];
-		//System.out.println(Date1 + "," + Date2);
+		// System.out.println(Date1 + "," + Date2);
 		date1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(finalDate1);
 		date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(finalDate2);
 		// SimpleDateFormat.parse(String);
 
-		//System.out.println(date1 + "-" + date2);
+		// System.out.println(date1 + "-" + date2);
 		Double dif = (double) (date1.getTime() - date2.getTime());
 		dif = dif / 1000;
 
@@ -314,132 +319,128 @@ public class Main implements Initializable {
 		Double minutes = dif / 60 % 60;
 		Double seconds = dif % 60;
 
-		//System.out.println(days + "," + hours + "," + minutes + "," + seconds);
+		// System.out.println(days + "," + hours + "," + minutes + "," +
+		// seconds);
 		result += (24 * days);
 		result += hours;
 		result += (minutes / 60);
 		result += (seconds / 360);
-	//	System.out.println(result);
+		// System.out.println(result);
 
 		return result;
 
 	}
-	public void updateProgrss(Double add){
+
+	public void updateProgrss(Double add) {
 		System.out.println(add);
-		
+
 		Double current = progress.getProgress();
 		System.out.println(current);
-		progress.setProgress(current+(add/100));
+		progress.setProgress(current + (add / 100));
 
 	}
-	
-	
-	public void updateData() throws IOException{ //download data from jira
+
+	public void updateData() throws IOException { // download data from jira
 		System.out.println("downloading...");
 		progress.setVisible(true);
-		
-		Thread th = new Thread(new Runnable(){
+
+		Thread th = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				progress.setProgress(0.0);
-				double percent = 100/Sites.size();
-				
-				for (String site : Sites){
-				if(site==Sites.get(Sites.size()-1)) // if last site, progress finished
-				{
-					updateProgrss(1.0);
+				double percent = 100 / Sites.size();
+
+				for (String site : Sites) {
+					if (site == Sites.get(Sites.size() - 1)) // if last site,
+																// progress
+																// finished
+					{
+						updateProgrss(1.0);
+					}
+
+					updateProgrss(percent);
+					String url = "https://jira.airobotics.co.il:8443/rest/api/2/issue/UCC-" + site
+							+ "?expand=changelog";
+					URL obj = null;
+					try {
+						obj = new URL(url);
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					String encoding = Base64.getEncoder().encodeToString(("Commandcenter:123456789").getBytes());
+					HttpURLConnection con = null;
+					try {
+						con = (HttpURLConnection) obj.openConnection();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					// optional default is GET
+					try {
+						con.setRequestMethod("GET");
+					} catch (ProtocolException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					con.setRequestProperty("Authorization", "Basic " + encoding);
+					// add request header
+					con.setRequestProperty("User-Agent", "Mozilla/5.0");
+					int responseCode = 0;
+					try {
+						responseCode = con.getResponseCode();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					System.out.println("\nSending 'GET' request to URL : " + url);
+					System.out.println("Response Code : " + responseCode);
+					BufferedReader in = null;
+					try {
+						in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					String inputLine;
+					StringBuffer response = new StringBuffer();
+					try {
+						while ((inputLine = in.readLine()) != null) {
+							response.append(inputLine);
+						}
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					try {
+						in.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					// print in String
+					System.out.println(response.toString());
+					// Read JSON response and print
+					// JSONObject myResponse = new
+					// JSONObject(response.toString());
+
+					try {
+						FileOutputStream fos = new FileOutputStream("./tickets/" + site + ".json");
+						fos.write(response.toString().getBytes());
+						fos.close();
+						// Log.d(TAG, "Written to file");
+					} catch (Exception e) {
+
+						e.printStackTrace();
+					}
+
 				}
-				
-				
-				updateProgrss(percent);
-				String url = "https://jira.airobotics.co.il:8443/rest/api/2/issue/UCC-"+site+"?expand=changelog";
-			     URL obj = null;
-				try {
-					obj = new URL(url);
-				} catch (MalformedURLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			     String encoding = Base64.getEncoder().encodeToString(("Commandcenter:123456789").getBytes());
-			     HttpURLConnection con = null;
-				try {
-					con = (HttpURLConnection) obj.openConnection();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			     // optional default is GET
-			     try {
-					con.setRequestMethod("GET");
-				} catch (ProtocolException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			     con.setRequestProperty ("Authorization", "Basic " + encoding);
-			     //add request header
-			     con.setRequestProperty("User-Agent", "Mozilla/5.0");
-			     int responseCode = 0;
-				try {
-					responseCode = con.getResponseCode();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			     System.out.println("\nSending 'GET' request to URL : " + url);
-			     System.out.println("Response Code : " + responseCode);
-			     BufferedReader in = null;
-				try {
-					in = new BufferedReader(
-					         new InputStreamReader(con.getInputStream()));
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			     String inputLine;
-			     StringBuffer response = new StringBuffer();
-			     try {
-					while ((inputLine = in.readLine()) != null) {
-					 	response.append(inputLine);
-					 }
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			     try {
-					in.close();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			     //print in String
-			     System.out.println(response.toString());
-			     //Read JSON response and print
-			     //JSONObject myResponse = new JSONObject(response.toString());
-			     
-			     try {      
-			         FileOutputStream fos = new FileOutputStream("./tickets/"+site+".json");
-			          fos.write(response.toString().getBytes());
-			           fos.close();      
-			         //Log.d(TAG, "Written to file");  
-			       } 
-			     catch (Exception e)
-			       {    
-			              
-			           e.printStackTrace(); 
-			        } 
-			     
-				} 
 			}
-				
-				
-			
-			
+
 		});
 		th.start();
-		
-		
-		
+
 	}
 }
